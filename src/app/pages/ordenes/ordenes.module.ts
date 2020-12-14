b1a56d4a09e9aed7e96bba2634f500bfa8f6f301
Gerardo Guevara <gerardoguevara64@gmail.com>
2020-12-13 19:25:34 -0600

    config pantalla ordenes

--- src/app/pages/ordenes/ordenes.module.ts
+++ src/app/pages/ordenes/ordenes.module.ts
@@ -1,14 +1,21 @@
+import { NgxPaginationModule } from 'ngx-pagination';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { OrdenesService } from './ordenes.service';
 import { ORDENES_ROUTES } from './ordenes.routes';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetalletOrdenComponent } from './detallet-orden/detallet-orden.component';
 import { OrdenesComponent } from './main-ordenes/ordenes.component';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModalFormComponent } from './modal-form/modal-form.component';
 
 
 
 @NgModule({
-  declarations: [ OrdenesComponent, DetalletOrdenComponent],
-  imports: [CommonModule, ORDENES_ROUTES],
+  declarations: [ OrdenesComponent, DetalletOrdenComponent, ModalFormComponent],
+  imports: [CommonModule, ORDENES_ROUTES,  FormsModule, NgxPaginationModule, NgbModalModule,
+    ReactiveFormsModule],
+  providers: [OrdenesService],
   exports: [ OrdenesComponent, DetalletOrdenComponent]
 })
 export class OrdenesModule { }
